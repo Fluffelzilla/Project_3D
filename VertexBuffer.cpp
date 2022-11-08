79649080ac72d603f91ccb3acff45b82446c413c
Fluffelzilla <carro.elsass@live.dk>
2022-11-08 11:36:00 +0100

    .

--- VertexBuffer.cpp
+++ VertexBuffer.cpp
@@ -5,18 +5,18 @@ VertexBuffer::VertexBuffer(ID3D11Device* device, UINT sizeOfVertex, UINT nrOfVer
 	vertexSize = sizeOfVertex;
 	nrOfVertices = nrOfVerticesInBuffer;
 
-	D3D11_BUFFER_DESC bufferDesc;
-	bufferDesc.ByteWidth = sizeof(vertexSize);
-	bufferDesc.Usage = D3D11_USAGE_IMMUTABLE;
-	bufferDesc.BindFlags = D3D11_BIND_VERTEX_BUFFER;
-	bufferDesc.CPUAccessFlags = 0;
-	bufferDesc.MiscFlags = 0;
-	bufferDesc.StructureByteStride = 0;
+	//D3D11_BUFFER_DESC bufferDesc;
+	//bufferDesc.ByteWidth = sizeof(vertexSize);
+	//bufferDesc.Usage = D3D11_USAGE_IMMUTABLE;
+	//bufferDesc.BindFlags = D3D11_BIND_VERTEX_BUFFER;
+	//bufferDesc.CPUAccessFlags = 0;
+	//bufferDesc.MiscFlags = 0;
+	//bufferDesc.StructureByteStride = 0;
 
-	D3D11_SUBRESOURCE_DATA data;
-	data.pSysMem = vertexData;
-	data.SysMemPitch = 0;
-	data.SysMemSlicePitch = 0;
+	//D3D11_SUBRESOURCE_DATA data;
+	//data.pSysMem = vertexData;
+	//data.SysMemPitch = 0;
+	//data.SysMemSlicePitch = 0;
 
 }
 
@@ -31,8 +31,7 @@ VertexBuffer::~VertexBuffer()
 
 void VertexBuffer::Initialize(ID3D11Device* device, UINT sizeOfVertex, UINT nrOfVerticesInBuffer, void* vertexData)
 {
-	
-	device->CreateBuffer(&bufferDesc, &data, &vertexBuffer);
+	device->CreateBuffer(&bufferDesc, &data, &buffer);
 }
 
 UINT VertexBuffer::GetNrOfVertices() const
