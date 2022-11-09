849600a9a9a78fc9f5a4e393bf0d9d065038652b
CAROLINE ELSASS <caea20@student.bth.se>
2022-11-09 18:09:50 +0100

    .

--- InputLayout.cpp
+++ InputLayout.cpp
@@ -1,5 +1,4 @@
 #include "InputLayout.h"
-//#include <cassert>
 
 InputLayout::~InputLayout()
 {
@@ -12,17 +11,13 @@ InputLayout::~InputLayout()
 
 void InputLayout::AddInputElement(const std::string& semanticName, DXGI_FORMAT format)
 {
-	//assert(format!=float3);
 	semanticNames.push_back(semanticName);
 	D3D11_INPUT_ELEMENT_DESC temp = { semanticName.c_str(), 0, format, 0, D3D11_APPEND_ALIGNED_ELEMENT, D3D11_INPUT_PER_VERTEX_DATA, 0 };
-	//obs ej alltid 3 floats ökning!! kan vara två float3 och sedan en float2 ex. position = float3, normal = float3, uvCoord = float2, försök fixa matten så det går och ta bort assert
 	elements.push_back(temp);
 }
 
 void InputLayout::FinalizeInputLayout(ID3D11Device* device, const void* vsDataPtr, size_t vsDataSize)
 {
-	//create input layout funktion är rätt, måste lista ut hur det implementeras på rätt sätt till min inputlayout...
-	//kommer min inputlayout få rätt data på detta vis?
 	device->CreateInputLayout(elements.data(), elements.size(), vsDataPtr, vsDataSize, &inputLayout);
 }
 
