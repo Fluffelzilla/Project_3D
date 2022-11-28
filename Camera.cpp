8450b87da329b8df01c33d40922f0f083d3bd83e
CAROLINE ELSASS <caea20@student.bth.se>
2022-11-28 11:03:01 +0100

    .

--- Camera.cpp
+++ Camera.cpp
@@ -2,7 +2,7 @@
 
 void Camera::MoveInDirection(float amount, const DirectX::XMFLOAT3& direction)
 {
-	position.x += direction.x*amount;
+	position.x += direction.x * amount;
 	position.y += direction.y * amount;
 	position.z += direction.z * amount;
 }
@@ -28,12 +28,12 @@ void Camera::Initialize(ID3D11Device* device, const ProjectionInfo& projectionIn
 
 	D3D11_BUFFER_DESC bufferDesc;
 	bufferDesc.ByteWidth = sizeof(cameraBuffer.GetSize());
-	bufferDesc.Usage = D3D11_USAGE_DYNAMIC; //can change values after created
-	bufferDesc.BindFlags = D3D11_BIND_CONSTANT_BUFFER; //defines whitch type of buffer to create, constant buffer is a type of buffer containing unstructured data
-	bufferDesc.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE; //CPU is allowed to write to this buffer
-	bufferDesc.MiscFlags = 0; // unused
-	bufferDesc.StructureByteStride = 0; // unused
-	device->CreateBuffer(&bufferDesc, NULL, &cameraBuffer.GetBuffer())
+	bufferDesc.Usage = D3D11_USAGE_DYNAMIC;
+	bufferDesc.BindFlags = D3D11_BIND_CONSTANT_BUFFER;
+	bufferDesc.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE;
+	bufferDesc.MiscFlags = 0;
+	bufferDesc.StructureByteStride = 0;
+	//device->CreateBuffer(&bufferDesc, NULL, &cameraBuffer.GetBuffer())
 }
 
 void Camera::MoveForward(float amount)
@@ -86,26 +86,25 @@ const DirectX::XMFLOAT3& Camera::GetUp() const
 
 void Camera::UpdateInternalConstantBuffer(ID3D11DeviceContext* context)
 {
-	
-	matrixInfo* dataptr;
+	//matrixInfo* dataptr;
 
-	D3D11_MAPPED_SUBRESOURCE mappedResource;
-	//fill mappedResource whit zero values, second input is the amound to set to zero
-	ZeroMemory(&mappedResource, sizeof(D3D11_MAPPED_SUBRESOURCE));
+	//D3D11_MAPPED_SUBRESOURCE mappedResource;
+	////fill mappedResource whit zero values, second input is the amound to set to zero
+	//ZeroMemory(&mappedResource, sizeof(D3D11_MAPPED_SUBRESOURCE));
 
-	context->Map(cameraBuffer.GetBuffer(), 0, D3D11_MAP_WRITE_DISCARD, 0, &mappedResource);
-	//get a pointer to the data contained in a subresource, and denies the GPU access to that subresource
+	//context->Map(cameraBuffer.GetBuffer(), 0, D3D11_MAP_WRITE_DISCARD, 0, &mappedResource);
+	////get a pointer to the data contained in a subresource, and denies the GPU access to that subresource
 
-	dataptr = (matrixInfo*)mappedResource.pData;
-	dataptr->world = DirectX::XMMatrixTranspose(mainCamera.getWorld());
-	dataptr->viewPro = mainCamera.getViewProjection();
-	dataptr->cPosition = mainCamera.getCameraPosition();
-	//new data input in CPU
+	//dataptr = (matrixInfo*)mappedResource.pData;
+	//dataptr->world = DirectX::XMMatrixTranspose(mainCamera.getWorld());
+	//dataptr->viewPro = mainCamera.getViewProjection();
+	//dataptr->cPosition = mainCamera.getCameraPosition();
+	////new data input in CPU
 
-	context->Unmap(cameraBuffer.GetBuffer(), 0);
-	//Lock new data att CPU and give acces back to the GPU
+	//context->Unmap(cameraBuffer.GetBuffer(), 0);
+	////Lock new data att CPU and give acces back to the GPU
 
-	context->VSSetConstantBuffers(0, 1, &cameraBuffer.GetBuffer());
+	//context->VSSetConstantBuffers(0, 1, &cameraBuffer.GetBuffer());
 }
 
 ID3D11Buffer* Camera::GetConstantBuffer() const
