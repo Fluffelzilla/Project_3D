af29b8180bac752ab93f6de6e93de85538347e40
CAROLINE ELSASS <caea20@student.bth.se>
2022-12-08 15:56:46 +0100

    .

--- SubMesh.cpp
+++ SubMesh.cpp
@@ -2,10 +2,20 @@
 
 void SubMesh::Initialize(size_t startIndexValue, size_t nrOfIndicesInSubMesh, ID3D11ShaderResourceView* ambientTextureSRV, ID3D11ShaderResourceView* diffuseTextureSRV, ID3D11ShaderResourceView* specularTextureSRV)
 {
+    ambientTexture = ambientTextureSRV;
+    diffuseTexture = diffuseTextureSRV;
+    specularTexture = specularTextureSRV;
+    startIndex = startIndexValue;
+    nrOfIndices = nrOfIndicesInSubMesh;
 }
 
 void SubMesh::PerformDrawCall(ID3D11DeviceContext* context) const
 {
+    //saknas något att rita, som är beroende av vertex data?
+    for (int i = 0; i < nrOfIndices; i++)
+    {
+        //context->Draw()
+    }
 }
 
 ID3D11ShaderResourceView* SubMesh::GetAmbientSRV() const
